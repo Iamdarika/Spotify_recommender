b485a71ab30b1c25abd611f49d757a2bc8f43a02
Aruna varshini N <arunavarshini2006@gmail.com>
2025-08-24 20:00:34 +0530

    Added OpenMP preprocessing placeholder

--- src/preprocess.c
+++ src/preprocess.c
@@ -1,1 +1,7 @@
+#include <stdio.h>
+#include <omp.h>
 
+void preprocess_data() {
+    // Placeholder for parallel preprocessing with OpenMP
+    printf("Preprocessing Spotify dataset with %d threads...\n", omp_get_max_threads());
+}
