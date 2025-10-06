e1f5194a98e0902f352c4da1e3dee2a55a25ed66
Aruna varshini N <arunavarshini2006@gmail.com>
2025-10-07 00:03:22 +0530

    Song.h
    Initial commit: Added basic Song structure with metadata fields (track_id, track_name, artists, genre_id).

--- Song.h
+++ Song.h
@@ -1,1 +1,16 @@
+#ifndef SONG_H
+#define SONG_H
+
+#include <string>
+
+struct Song {
+    std::string track_id;
+    std::string track_name;
+    std::string artists;
+    int genre_id;
+
+    Song() : genre_id(-1) {}
+};
+
+#endif // SONG_H
 
