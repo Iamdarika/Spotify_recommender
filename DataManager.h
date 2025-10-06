9b2d56997e06b033503ff43c7d600a37b34bd4b5
Aruna varshini N <arunavarshini2006@gmail.com>
2025-10-07 01:04:12 +0530

    initial  DataManager.h
    Initial commit: Added basic DataManager class skeleton with function declarations for data preprocessing and loading.

--- DataManager.h
+++ DataManager.h
@@ -1,1 +1,13 @@
+#ifndef DATAMANAGER_H
+#define DATAMANAGER_H
+
+#include <string>
+
+class DataManager {
+public:
+    static bool preprocessData(const std::string& csvPath, const std::string& outputPath);
+    static bool loadData(const std::string& binaryPath);
+};
+
+#endif // DATAMANAGER_H
 
